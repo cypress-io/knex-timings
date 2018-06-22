6b25400f71e2b81ec9f38eabdf9ee1a009d29504
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-22 06:40:51 -0400

    initial code

---
+++ src/knex-timings-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* eslint-env mocha */
+const knexTimings = require('.')
+describe('knex-timings', () => {
+  it('write this test', () => {
+    console.assert(knexTimings, 'should export something')
+  })
+})
